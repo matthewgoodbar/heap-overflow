9d4c1d31ed27a2fa6d2dedbe171903e8d6a1c94b
matthew <matthewgoodbar@gmail.com>
2022-12-29 14:21:22 -0800

    Add create action for users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,15 @@
 class Api::UsersController < ApplicationController
 
+  wrap_parameters include: User.attribute_names + ['password']
+
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render json: { user: @user }
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
