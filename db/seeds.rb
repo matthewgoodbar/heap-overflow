8ec57df11f945afacbc71a0f33e66f9df6fe426e
matthew <matthewgoodbar@gmail.com>
2022-12-29 12:26:07 -0800

    Add seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+ApplicationRecord.transaction do 
+    puts "Destroying tables..."
+    User.destroy_all
+  
+    puts "Resetting primary keys..."
+    ApplicationRecord.connection.reset_pk_sequence!('users')
+  
+    puts "Creating users..."
+    # Create one user with an easy to remember username, email, and password:
+    User.create!(
+      username: 'demo', 
+      email: 'demo@heap-overflow.com', 
+      password: 'password'
+    )
+  
+    # More users
+    10.times do 
+      User.create!({
+        username: Faker::Internet.unique.username(specifier: 3),
+        email: Faker::Internet.unique.email,
+        password: 'password'
+      }) 
+    end
+  
+    puts "Done!"
+  end
