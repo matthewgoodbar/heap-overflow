91d5b437ddd0b77342ed75df132e56a129c9c314
matthew <matthewgoodbar@gmail.com>
2022-12-29 11:52:05 -0800

    Fix validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,16 @@
 #
 class User < ApplicationRecord
     attr_reader :password
-    validates :username, :email, :session_token, presence: true, uniqueness: true
-    validates :password_digest, presence: true
-    validates :password, length: {minimum: 6}, allow_nil: true
+    validates :username, 
+        uniqueness: true, 
+        length: { in: 3..30 }, 
+        format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
+    validates :email, 
+        uniqueness: true, 
+        length: { in: 3..255 }, 
+        format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length: { in: 6..255 }, allow_nil: true
 
     before_validation :ensure_session_token
 
