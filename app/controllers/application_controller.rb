427bc3ef44306079f4199c7ba527ba50a576d8fe
matthew <matthewgoodbar@gmail.com>
2022-12-29 12:27:48 -0800

    Add cookie support to application.rb, params formatting to environment.rb, application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::API
+
+    before_action :snake_case_params
+    
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def require_logged_in
+
+    end
+
+    def require_logged_out
+    end
+
+    def logged_in?
+    end
+
+    def login(user)
+    end
+
+    def logout
+    end
+
+    private
+    def snake_case_params
+        params.deep_transform_keys!(&:underscore)
+    end
 end
--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,11 @@ module HeapOverflow
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore,
+      key: '_heap_overflow_session',
+      same_site: :lax, 
+      secure: Rails.env.production?
   end
 end
