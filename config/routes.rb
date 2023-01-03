fcb2d849ca1787e09de0430de2d0419b8fd03329
matthew <matthewgoodbar@gmail.com>
2023-01-03 11:50:43 -0800

    Add routes, empty controller, jbuilder views for questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
+    resources :questions, only: [:index, :show, :create, :update :destroy]
     resource :session, only: [:show, :create, :destroy]
   end
 end
