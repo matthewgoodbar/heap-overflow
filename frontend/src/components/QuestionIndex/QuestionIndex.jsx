41549db2859f9d43ceadd7830526e168192331df
matthew <matthewgoodbar@gmail.com>
2023-01-03 18:07:23 -0800

    Work on question index page functionality

--- frontend/src/components/QuestionIndex/QuestionIndex.jsx
+++ frontend/src/components/QuestionIndex/QuestionIndex.jsx
@@ -1,16 +1,22 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Redirect, useParams } from "react-router-dom";
 import { fetchQuestions } from "../../store/question";
 
 const QuestionIndex = props => {
 
     const dispatch = useDispatch();
     const questions = useSelector(state => Object.values(state.questions));
-
+    const { page } = useParams();
+    console.log(page);
+    
     useEffect(() => {
         dispatch(fetchQuestions());
     }, []);
+
+    // if (!page) return <Redirect to="/questions?page=1"/>
     
+    // if (page) 
     return (
         <div id="question-index" className="component-standard">
             <h2>All Questions</h2>
@@ -20,6 +26,7 @@ const QuestionIndex = props => {
             </ul>
         </div>
     );
+    // else return <Redirect to="/questions?page=1"/>;
 };
 
 export default QuestionIndex;
