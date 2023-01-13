01ad55abd3658ff1208c3b90019970bc7568a1ac
matthew <matthewgoodbar@gmail.com>
2023-01-12 23:40:47 -0800

    Add footer links, add to splash, login/signup form fix

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,23 @@
+import { Link } from "react-router-dom";
 
 const Footer = props => {
     return (
         <footer id="footer">
-
+            <div className="footer-compartment">
+                <Link to="/">
+                    <img className="logo-image" alt="heap-overflow-logo" />
+                </Link>
+            </div>
+            <div className="footer-compartment footer-compartment-border">
+                <Link className="footer-link" to="/login">Log In</Link>
+                <Link className="footer-link" to="/signup">Sign Up</Link>
+                <Link className="footer-link" to="/questions">Questions</Link>
+            </div>
+            <div className="footer-compartment footer-compartment-border">
+                <Link className="footer-link" to="/404">About</Link>
+                <Link className="footer-link" to="/404">Github</Link>
+                <Link className="footer-link" to="/404">LinkedIn</Link>
+            </div>
         </footer>
     );
 };
--- frontend/src/components/Splash/Splash.jsx
+++ frontend/src/components/Splash/Splash.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Splash = props => {
     
@@ -12,19 +12,38 @@ const Splash = props => {
     
     return (
         <div id="splash" className="component-standard">
-            <h1>Welcome to Heap Overflow!</h1>
+            <h1 className="splash-header">Welcome to Heap Overflow!</h1>
             <br/>
-            <p>This is the sample splash page. Click 'Log In' to log in if you have an existing account, <br/>
-                or 'Sign Up' if you don't have an account yet.
+            <p className="splash-text">This is a forum where you can ask questions and<br/>
+                receive answers from dozens of users across the globe.<br/>
             </p>
             <br/>
-            {currentUser &&
+            <p className="splash-text splash-bold">Have a burning question?</p>
+            <p className="splash-text">
+                Type your query into the search bar at the top to see if<br/>
+                anyone else has been in your shoes before.
+            </p>
+            <br/>
+            <p className="splash-text splash-bold">No one has asked your question yet?</p>
+            <p className="splash-text">Feel free to <Link to="/login">log in</Link> or <Link to="/signup">sign up</Link> to
+                ask your <br/>
+                incredibly unique question.
+            </p>
+            <br/>
+            <p className="splash-text splash-bold">Waiting for the answers to come rolling in?</p>
+            <p className="splash-text">Spend some time browsing other user's questions!<br/>
+                You might just make someone's day,<br/>
+                it's the least you can do!    
+            </p>
+            <br/>
+            <br/>
+            {/* {currentUser &&
             <>
-                <p>You are currently logged in as: {currentUser.username}</p>
+                <p className="splash-text">You are currently logged in as: {currentUser.username}</p>
                 <br/>
             </>
-            }
-            <NavLink to="/questions" className="button-light">Click here to view questions</NavLink>
+            } */}
+            <Link to="/questions" className="button-dark splash-button">Browse all questions</Link>
         </div>
     );
 };
