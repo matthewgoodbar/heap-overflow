1a8167fe6abd420aff5f22727b22e204841f89b0
matthew <matthewgoodbar@gmail.com>
2023-02-01 15:39:41 -0800

    Add other projects to sidebar

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -11,8 +11,14 @@ const Sidebar = props => {
                 <NavLink to="/users" activeClassName="sidebar-selected" className="sidebar-link">Users</NavLink>
                 <div className="sidebar-projects">
                     <p className="sidebar-text">My other projects</p>
-                    <a href="https://matthewgoodbar.github.io/js-game" className="sidebar-text">CATACOMB</a>
-                    <a href="https://friend-ly.onrender.com" className="sidebar-text"></a>
+                    <a href="https://matthewgoodbar.github.io/js-game" className="sidebar-text" target="_blank">CATACOMB</a>
+                    <p className="sidebar-text">
+                        An action survival browser game made in vanilla JavaScript
+                    </p>
+                    <a href="https://friend-ly.onrender.com" className="sidebar-text" target="_blank">Friend.ly</a>
+                    <p className="sidebar-text">
+                        A group project chat app made using the MERN stack
+                    </p>
                 </div>
                 <div className="sidebar-logo-links">
                     <a href="https://www.linkedin.com/in/matthew-goodbar-671a24169/" target="_blank"><img className="linkedin-image-gray" alt="linkedin logo" /></a>
