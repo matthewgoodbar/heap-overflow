e930ae66581e7c456e5fae433d97d8694cc0ac43
matthew <matthewgoodbar@gmail.com>
2023-02-09 13:45:31 -0800

    Fill out about page

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -3,8 +3,12 @@ const About = props => {
     return (
         <div id="about" className="component-with-sidebar">
             <h1>About Heap Overflow</h1>
-            <p>This website was made as a solo project in ~2 weeks</p>
-            <p>It utilizes a React.js + Redux frontend and a Ruby on Rails backend</p>
+            <p>This website was made as a solo project in ~2 weeks, and utilizes</p>
+            <p>a React.js + Redux frontend and a Ruby on Rails backend.</p>
+            <p>For more details on technical implementation, </p>
+            <p>
+                check out the README on this project's <a href="https://github.com/matthewgoodbar/heap-overflow">github repo</a>.
+            </p>
         </div>
     );
 };
